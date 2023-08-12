1fa52cd0e0c51d8702a170eb34a23fdf541c7e6c
MGHefny <mgenedhefny@gmail.com>
2023-08-11 19:32:49 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description:  numberz.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 35; n < 45; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
