c6d2d5f88d645236e200ebec37455aef084baf91
MGHefny <mgenedhefny@gmail.com>
2023-08-11 19:42:11 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description:  prints the alphabet in lowercase.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int x;
+	char c[6] = "abcdef";
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (x = 0; x < 6; x++)
+	{
+		putchar(alp[x]);
+	}
+	putchar('\n');
+	return (0);
+}
