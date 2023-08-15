3c7f8c4df219873d725e45eaa343fc59f4150e8b
MGHefny <mgenedhefny@gmail.com>
2023-08-15 16:13:48 -0700

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description:   prints _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar('c'"\n");
+	}
+	return (0);
+}
