19f8edd056d489dec18f75ebff11e608297b1bac
MGHefny <mgenedhefny@gmail.com>
2023-08-17 18:52:55 -0700

    function 1x4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - numb except 2 and 4
+ *
+ * return: function
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 58; c++)
+	{
+		if (c != 50 || c != 52)
+			_putchar(c);
+	}
+	_putchar('\n);
+}
