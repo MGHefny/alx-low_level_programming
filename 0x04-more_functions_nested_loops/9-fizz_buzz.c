7ef3bfbf62b1443f8fd40a4d29e1ad80e8209f76
MGHefny <mgenedhefny@gmail.com>
2023-08-17 19:19:45 -0700

    function 1x9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-	int i;
+	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		} else if (x % 5 == 0 && x % 3 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		} else if (x % 3 == 0 && x % 5 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		} else if (x == 1)
 		{
 			printf("%d", x);
