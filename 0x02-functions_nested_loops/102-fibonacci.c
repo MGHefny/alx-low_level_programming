fc3cdd71fa19c5bcfaa9fd283a46bd71a260c19d
MGHefny <mgenedhefny@gmail.com>
2023-08-18 12:04:57 -0700

    function adv 0x2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,13 +11,13 @@ int main(void)
 	int x;
 	unsigned long f1 = 0, f2 = 1, sum;
 
-	for (x = 0; x < 100; x++)
+	for (x = 0; x < 50; x++)
 	{
 		sum = f1 + f2;
 		printf("%lu", sum);
 		f1 = f2;
 		f2 = sum;
-		if (x == 99)
+		if (x == 49)
 			printf("\n");
 		else
 			printf(", ");
