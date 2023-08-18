13f072b8a67e113c3f34e250947e5577e23586a8
MGHefny <mgenedhefny@gmail.com>
2023-08-17 18:44:35 -0700

    function 1x 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - number 0-9
+ * return: function
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 58, c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
