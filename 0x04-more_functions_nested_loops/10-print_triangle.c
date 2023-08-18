5cae25bef989d85e5390a8ba5cfbd0d5c313de55
MGHefny <mgenedhefny@gmail.com>
2023-08-17 19:23:13 -0700

    function 1x10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: size triangle
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = x; y < size; y++)
+			{
+				_putchar(' ');
+			}
+
+			for (y = 1; y <= i; y++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
+
