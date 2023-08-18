1439d812b107f9b0c41939e2ddd14cf0bf2c5ef8
MGHefny <mgenedhefny@gmail.com>
2023-08-17 18:33:10 -0700

    function 1x2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 48;
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 97;
+	printf("%c: %d\n", c, _isdigit(c));
+	return (0);
+}
