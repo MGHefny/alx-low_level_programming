a94f39c3a618fa5279ad2e2011bc8549b07fc05e
MGHefny <mgenedhefny@gmail.com>
2023-08-21 15:37:32 -0700

    pointer and array 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _strlen - lenth of string
+  * @s: string
+  * Return: the length
+  */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '0')
+	{
+		len++;
+		s++;
+	}
+	return (len)
+}
