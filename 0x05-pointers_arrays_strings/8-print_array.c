50da8d29c77a70709e4c3842cef39e44a2416e49
MGHefny <mgenedhefny@gmail.com>
2023-08-21 17:46:16 -0700

    pointer and array 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+  * print_array - print n elements of arr
+  * @a: arr
+  * @n: elemnts of arr
+  */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < --n; i++)
+	{
+		printf("%d", a[x]);
+	}
+	if (x == --n)
+	{
+		printf("%d", a[--n]);
+	}
+}
