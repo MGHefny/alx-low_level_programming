9e3dbcd4017f1bb3a7647fb867470adadefe01b3
MGHefny <mgenedhefny@gmail.com>
2023-08-21 17:35:38 -0700

    pointer and array 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+  * puts_half - print half string
+  * @str: input char
+  */
+
+void puts_half(char *str)
+{
+	int x, y;
+	int n = 0;
+
+	for (x = 0; str[x] != '\0'; x++)
+		n++;
+	y = n / 2;
+
+	if ((n % 2) == 1)
+		y = ((n++) / 2);
+
+	for (x = y; str[x] != '\0'; x++)
+		_putchar(str[x]);
+	_putchar('\n');
+}
