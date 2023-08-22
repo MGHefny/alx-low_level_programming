fddf3ad32566eaf47998ef070a111c82a79acd6a
MGHefny <mgenedhefny@gmail.com>
2023-08-21 18:37:09 -0700

    pointer and arr adv 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,18 +9,21 @@
 
 int main(void)
 {
-	int pass[50];
+	int pass[100];
 	int x, y;
-	int sum = 0;
 
-	for (x = 0; x < 50; x++)
+	int sum = 0;	
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
 	{
-		pass[x] = rand() % 39;
+		pass[x] = rand() % 78;
 		sum += (pass[x] + '0');
 		putchar(pass[x] + '0');
-		if ((1386 - sum) - '0' < 39)
+		if ((2772 - sum) - '0' < 78)
 		{
-			y = 1386 - sum - '0';
+			y = 2772 - sum - '0';
 			sum += y;
 			putchar(y + '0');
 			break;
