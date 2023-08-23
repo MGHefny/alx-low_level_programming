59451b0ba8c709d7409d779af7270d40dbfaae3c
MGHefny <mgenedhefny@gmail.com>
2023-08-23 16:59:49 -0700

    0x06 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,21 +11,17 @@ char *cap_string(char *str)
 
 	while (str[x])
 	{
-
-
-		if (str[x - 1] == ' ' || str[x - 1] == '\t' ||
-		    str[x - 1] == '\n' || str[x - 1] == ',' ||
-		    str[x - 1] == ';' || str[x - 1] == '.' ||
-		    str[x - 1] == '!' || str[x - 1] == '?' ||
-		    str[x - 1] == '"' || str[x - 1] == '(' ||
-		    str[x - 1] == ')' || str[x - 1] == '{' ||
-		    str[x - 1] == '}' || x == 0)
-			str[x] -= 32;
-
-
 		while (!(str[x] >= 'a' && str[x] <= 'z'))
 			x++;
 
+		if (str[x - 1] == ' ' || str[x - 1] == '{' ||
+				str[x - 1] == '}' || str[x - 1] == '!' ||
+				str[x - 1] == '\t' || str[x - 1] == ';' ||
+				str[x - 1] == '\n' || str[x - 1] == '.' ||
+				str[x - 1] == ')' || str[x - 1] == '?' ||
+				str[x - 1] == ',' || str[x - 1] == '(' ||
+				str[x - 1] == '"' || x == 0)
+			str[x] -= 32;
 
 		x++;
 	}
