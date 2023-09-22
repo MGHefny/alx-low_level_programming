1f79b62a7b1426c95ec0ae15935606727881192e
MGHefny <mgenedhefny@gmail.com>
2023-09-21 19:14:46 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,14 +11,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	unsigned int len = 0;
-	
+
 	while (str[len])
-		l++;
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
-
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
