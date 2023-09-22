724f10a5d98916b5caa3c707d7e47db90298ecd5
MGHefny <mgenedhefny@gmail.com>
2023-09-21 18:38:02 -0700

    0x12 full

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,26 +9,25 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *n;
-	list_t *t = *head;
-	unsigned int l = 0;
-
-	while (str[l])
-		l++;
-	n = malloc(sizeof(list_t));
-	if (!n)
-		return (NULL);
-	n->str = strdup(str);
-	n->l = l;
-	n->next = NULL;
-	if (*head == NULL)
-	{
-		*head = n;
-		return (n);
-	}
-	while (t->next)
-		t = t->next;
-	t->next = n;
-	return (n);
-}
+    list_t *new;
+    list_t *temp = *head;
+    unsigned int l = 0;
+    while (str[l])
+    l++;
+    new = malloc(sizeof(list_t));
+    if (!new)
+    return (NULL);
+    new->str = strdup(str);
+    new->l = l;
+    new->next = NULL;
+    if (*head == NULL)
+    {
+        *head = new;
+        return (new);
+        }
+        while (temp->next)
+        temp = temp->next;
+        temp->next = new;
+        return (new);
+        }
 
