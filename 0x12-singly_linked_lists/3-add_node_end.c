3fd8f8b5675d4fa86875d4b296dc7821229c9458
MGHefny <mgenedhefny@gmail.com>
2023-09-21 18:34:09 -0700

    0x12 full

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,28 +9,26 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
- list_t *n;
- list_t *t = *head;
- unsigned int l = 0;
+	list_t *n;
+	list_t *t = *head;
+	unsigned int l = 0;
 
- while (str[l])
- l++;
- n = malloc(sizeof(list_t));
- if (!n)
- return (NULL);
- n->str = strdup(str);
- n->l = l;
- n->next = NULL;
-
- if (*head == NULL)
- {
- *head = n;
- return (n);
- }
-
- while (t->next)
- t = t->next;
- t->next = n;
- return (n);
+	while (str[l])
+		l++;
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+	n->str = strdup(str);
+	n->l = l;
+	n->next = NULL;
+	if (*head == NULL)
+	{
+		*head = n;
+		return (n);
+	}
+	while (t->next)
+		t = t->next;
+	t->next = n;
+	return (n);
 }
 
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,14 @@
  */
 void free_list(list_t *head)
 {
- list_t *t;
+	list_t *t;
 
- while (head)
- {
- t = head->next;
- free(head->str);
- free(head);
- head = t;
- }
+	while (head)
+	{
+		t = head->next;
+		free(head->str);
+		free(head);
+		head = t;
+	}
 }
 
