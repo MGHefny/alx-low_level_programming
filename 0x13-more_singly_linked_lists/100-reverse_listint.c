9c3667ae7726f582234bef08caa7f7b4a6bfe8eb
MGHefny <mgenedhefny@gmail.com>
2023-09-26 18:44:29 -0700

    adv 1

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,17 +7,17 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *n = NULL;
-	listint_t *p = NULL;
+	listint_t *pr = NULL;
 
 	while (*head)
 	{
 		n = (*head)->n;
-		(*head)->n = p;
-		p = *head;
+		(*head)->n = pr;
+		pr = *head;
 		*head = n;
 	}
 
-	*head = p;
+	*head = pr;
 
 	return (*head);
 }
