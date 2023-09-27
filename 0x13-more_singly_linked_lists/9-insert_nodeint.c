fd42955cacba55e83c7fd0b75f685ebaf9d29ae5
MGHefny <mgenedhefny@gmail.com>
2023-09-26 18:17:52 -0700

    9 0x13

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,38 +1,39 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - insert x ele postiev 
+ * insert_nodeint_at_index - insert x ele postiev
  * @head: first node
  * @n: data new node
  * @idx: index node add
  * Return: new node or null
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x;
-	listint_t *nw;
+	listint_t *new;
 	listint_t *t = *head;
 
-	nw = malloc(sizeof(listint_t));
-	if (!nw || !head)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	nw->n = n;
-	nw->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		nw->next = *head;
-		*head = nw;
-		return (nw);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
 	for (x = 0; t && x < idx; x++)
 	{
 		if (x == idx - 1)
 		{
-			nw->next = t->next;
-			t->next = nw;
-			return (nw);
+			new->next = t->next;
+			t->next = new;
+			return (new);
 		}
 		else
 			t = t->next;
