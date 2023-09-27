e5c10e619cc11ed974fc0643ede8a16c719e39cd
MGHefny <mgenedhefny@gmail.com>
2023-09-26 17:55:09 -0700

    9 0x13

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef LISTS_H
+i#ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,4 +26,5 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 #endif
