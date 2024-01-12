b1df96a959983a328ce415629c20af5391f428e8
MGHefny <mgenedhefny@gmail.com>
2024-01-12 03:50:33 +0200

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.x"
+/**
+ * add_dnodeint - add node in beginning
+ * @head: head list
+ * @n: value
+ * Return: address n_x element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *n_x;
+	dlistint_t *x;
+
+	n_x = malloc(sizeof(dlistint_t));
+	if (n_x == NULL)
+		return (NULL);
+
+	n_x->n = n;
+	n_x->prev = NULL;
+	x = *head;
+
+	if (x != NULL)
+	{
+		while (x->prev != NULL)
+			x = x->prev;
+	}
+
+	n_x->next = x;
+
+	if (x != NULL)
+		x->prev = n_x;
+
+	*head = n_x;
+	return (n_x);
+}
