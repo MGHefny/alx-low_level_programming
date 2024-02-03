a1b23c3067e3525ceeec632b3a0301c1079c4ff4
MGHefny <mgenedhefny@gmail.com>
2024-02-03 13:31:25 +0200

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get the index at which
+ * @key:  key to get the index 
+ * @size: size the array of the hash table
+ *
+ * Return: index  the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
