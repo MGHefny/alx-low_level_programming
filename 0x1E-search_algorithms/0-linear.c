1d2c4f11da6e292544fbb48288b1644d160d1d53
MGHefny <mgenedhefny@gmail.com>
2024-05-09 01:23:57 +0300

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,14 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-	int x;
+	size_t x;
 
 	if (array == NULL)
 		return (-1);
 
-	for (x = 0; x < (int)size; x++)
+	for (x = 0; x < size; x++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+		printf("Value checked array[%d] = [%d]\n", x, array[x]);
 		if (array[x] == value)
 			return (x);
 	}
