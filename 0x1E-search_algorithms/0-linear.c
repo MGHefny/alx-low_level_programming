c9799a00b01c7de4a9a99629f90cac431ccafd0e
MGHefny <mgenedhefny@gmail.com>
2024-05-09 01:11:04 +0300

    0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - searche for value in array
+  * @array: pointer first element
+  * @size: numb element
+  * @value: value
+  *
+  * Return: NULL or -1.
+  * Description: Print value every time
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,49 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * listint_s - singly linked list
+ * @x: Int
+ * @ind: Index node
+ * @nxt: Pointer next node
+ *
+ * Description: singly linked list
+ */
+typedef struct listint_s
+{
+		int x;
+		size_t ind;
+		struct listint_s *nxt;
+} listint_t;
+
+/**
+ * skiplist_s - singly linked list
+ *
+ * @x: Int
+ * @ind: Index node
+ * @nxt: Pointer next node
+ * @ex: Pointer next node
+ * Description: singly linked list
+ */
+typedef struct skiplist_s
+{
+				int x;
+				size_t ind;
+				struct skiplist_s *nxt;
+				struct skiplist_s *ex;
+} skiplist_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+
+#endif /* SEARCH_ALGO_H */
